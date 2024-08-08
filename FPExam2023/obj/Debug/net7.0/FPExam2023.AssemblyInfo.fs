90a55adcc8a5596dcc4bb62add33da8701a993eb
Mikto97 <[email protected]>
2024-08-08 18:35:30 +0200

    asdsd

--- FPExam2023/obj/Debug/net7.0/FPExam2023.AssemblyInfo.fs
+++ FPExam2023/obj/Debug/net7.0/FPExam2023.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("FPExam2023")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+076c00e7dbb5ba3d9785e0b2da6a266ec1dacd92")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56a4da3a03b1119ae2df1353794bd6c5e838c6b0")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("FPExam2023")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("FPExam2023")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
